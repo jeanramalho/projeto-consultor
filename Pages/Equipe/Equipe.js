7152a7b2106143aaa970fa46459d85f56e8c2ce3
jeanramalho <jeanramalho.dev@gmail.com>
2023-02-28 15:04:28 -0300

    Adiciona javascript da página equipe

---
+++ Pages/Equipe/Equipe.js
@@ -0,0 +1,10 @@
+$('.carousel').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+  });
