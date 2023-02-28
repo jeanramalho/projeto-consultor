69df6e204bb691c21f71e19ccdeadd0aa792d267
jeanramalho <jeanramalho.dev@gmail.com>
2023-02-28 15:20:56 -0300

    Implementa carrossel com exemplos

--- Pages/Equipe/Equipe.js
+++ Pages/Equipe/Equipe.js
@@ -2,7 +2,7 @@ $('.carousel').slick({
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 4,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
