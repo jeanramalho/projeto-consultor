7c34d83de5ed8a03d21785264456d3fa865107ef
jeanramalho <jeanramalho.dev@gmail.com>
2023-02-28 15:20:35 -0300

    ponto 1

--- Pages/Equipe/Equipe.js
+++ Pages/Equipe/Equipe.js
@@ -2,7 +2,7 @@ $('.carousel').slick({
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
